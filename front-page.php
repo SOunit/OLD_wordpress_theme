9b5602905316c48884c853471c1a04509c0e32f3
SOunit <workbookwork7@gmail.com>
2021-11-13 12:36:15 -0800

    initial commit

---
+++ front-page.php
@@ -0,0 +1,3 @@
+<?php get_header(); ?>
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio, ut architecto autem repudiandae, error repellat possimus fugiat, molestias necessitatibus eaque eligendi. Ut temporibus dolorem nobis velit alias culpa repudiandae ea.
+<?php get_footer(); ?>
